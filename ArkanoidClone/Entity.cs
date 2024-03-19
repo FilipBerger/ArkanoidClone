bb48b2c4db11437b070b3b518f7c4e87018fd875
Filip <filip-berger@hotmail.com>
2024-03-19 10:05:25 +0100

    Add project files.

---
+++ ArkanoidClone/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArkanoidClone
+{
+    public class Entity
+    {
+        private int y;
+        private int x;
+        private int height;
+        private int width;
+    }
+}
---
+++ ArkanoidClone/SizeUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArkanoidClone
+{
+    public class SizeUp : PowerUps
+    {
+        public void ApplyEffect()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
