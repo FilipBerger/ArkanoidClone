33dea990548badbfcdd059ec010d2d5ec1d388aa
Filip <filip-berger@hotmail.com>
2024-04-01 15:32:22 +0200

    WIP

--- ArkanoidClone/HighScoreScreen.cs
+++ ArkanoidClone/HighScoreScreen.cs
@@ -6,20 +6,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
 namespace ArkanoidClone
 {
     public class HighScoreScreen
     {
-        private List<HighScore> highScores = new List<HighScore>();
+        private List<HighScore> highScores;
         private SpriteFont font;
         private Vector2 position = new Vector2(100, 100);
 
-        public HighScoreScreen(SpriteFont font, string jsonFilePath)
+        public HighScoreScreen(SpriteFont font)
         {
             this.font = font;
-            highScores = HighScoresManager.LoadHighScores(jsonFilePath);
+            highScores = HighScoreManager.LoadHighScores();
         }
-
-       
     }
 }
