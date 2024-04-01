bb6d5db18e1ba685532f7930d45ff0c82f957d0b
Filip <filip-berger@hotmail.com>
2024-04-01 14:02:50 +0200

    Created a HighScoresList class.

---
+++ ArkanoidClone/HighScoresList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkanoidClone
+{
+    internal class HighScoresList
+    {
+        private List<HighScore> highScores = new List<HighScore>();
+
+        public List<HighScore> HighScores
+        {
+            get { return highScores; }
+            set { highScores = value; }
+        }
+    }
+}
