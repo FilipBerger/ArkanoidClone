f869b0cfb933280d10c8be88557c33b90d242bf9
Johohoa <johnny.dahle@hotmail.com>
2024-04-03 11:42:47 +0200

    Created a separate class for keeping track of points.

---
+++ ArkanoidClone/ScoreManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkanoidClone
+{
+    public class ScoreManager
+    {
+        private int score;
+        private int brickHitPoints;
+        private int enemyHitPoints;
+
+        public ScoreManager (int brickHitPoints, int enemyHitPoints)
+        {
+            this.brickHitPoints = brickHitPoints;
+            this.enemyHitPoints = enemyHitPoints;
+            this.score = 0;
+        }
+
+        public void BrickHit()
+        {
+            score += brickHitPoints;
+        }
+
+        public void EnemyHit()
+        {
+            score += enemyHitPoints;
+        }
+
+        public void TimeBonus(int timeBonus)
+        {
+            score += timeBonus;
+        }
+
+        public int GetScore()
+        {
+            return score;
+        }
+    }
+}
