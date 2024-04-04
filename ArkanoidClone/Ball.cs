359a827128f7e4bdc55a234f9d37897e712331a5
kevinmartiin <nextgenallu@hotmail.com>
2024-04-04 14:46:03 +0200

    Updated logic for bricks, removed obsolete/not used code

--- ArkanoidClone/Ball.cs
+++ ArkanoidClone/Ball.cs
@@ -6,8 +6,7 @@ using System;
 
 public class Ball : Entity
 {
-    public Vector2 Velocity { get; set; } 
-
+    public Vector2 Velocity { get; set; }
 
     public Ball(Texture2D texture, Vector2 position, Vector2 velocity, Rectangle boundingBox) : base(texture, position, 0, boundingBox) // Hastighet sätts till 0 eftersom hastigheten kommer att styras av Velocity-egenskapen
     {
@@ -16,12 +15,9 @@ public class Ball : Entity
 
     public void Update(GameTime gameTime, List<Entity> entities)
     {
-
         Position += Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-
         BoundingBox = new Rectangle((int)Position.X, (int)Position.Y, BoundingBox.Width, BoundingBox.Height);
-
 
         foreach (Entity entity in entities)
         {
@@ -37,6 +33,7 @@ public class Ball : Entity
     private void HandleCollision(Entity entity, GameTime gameTime)
     {
         Position -= Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
         Vector2 oldPosition = Position;
 
         if (entity is Wall)
@@ -73,58 +70,24 @@ public class Ball : Entity
                 // Ändra riktningen vertikalt
                 Velocity = new Vector2(Velocity.X, -Velocity.Y);
             }
-
-            ////Senaste version Bollen fastnar/studsar mellan bricks
-            //if (BoundingBox.Intersects(entity.BoundingBox))
-            //{
-            //    float intersectX = BoundingBox.Center.X - entity.BoundingBox.Center.X;
-            //    float normalizedIntersectX = intersectX / entity.BoundingBox.Width;
-            //    float angle = normalizedIntersectX * MathHelper.ToRadians(60);
-
-            //    Vector2 newVelocity = Vector2.Transform(Velocity, Matrix.CreateRotationZ(angle));
-            //    Velocity = newVelocity;
-
-            //    Position = oldPosition + Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            //}
-
-            ////Tidigare Version
-            ////Velocity = new Vector2(Velocity.X, -Velocity.Y);
-            //Rectangle entityRect = entity.BoundingBox; // Antag att BoundingBox är en Rectangle
-
-            //// Kolla om bollens bounding box interagerar med botten av entitetens bounding box
-            //if (BoundingBox.Intersects(new Rectangle(entityRect.Left, entityRect.Bottom, entityRect.Width, 1)))
-            //{
-            //    // Här kan du hantera logiken för när bollen träffar botten av entiteten
-            //    Velocity = new Vector2(Velocity.X, -Velocity.Y);
-            //}
-            //else
-            //{
-            //    Velocity = new Vector2(-Velocity.X, Velocity.Y);
-            //}
         }
         else if (entity is PlayerBar)
         {
             float intersectX = BoundingBox.Center.X - entity.BoundingBox.Center.X;
+
             float normalizedIntersectX = intersectX / entity.BoundingBox.Width;
+
             float angle = normalizedIntersectX * MathHelper.ToRadians(60); // 60 grader är en vanlig vinkeländring i Arkanoid
+
 
             Vector2 newVelocity = Vector2.Transform(Velocity, Matrix.CreateRotationZ(angle));
 
+
             float newVelocityX = (float)Math.Sin(angle) * Velocity.Length();
+
             float newVelocityY = -(float)Math.Cos(angle) * Velocity.Length();
+
             Velocity = new Vector2(newVelocityX, newVelocityY);
-
-            ////float relativeIntersectX = (BoundingBox.Center.X - entity.BoundingBox.Left) / (float)entity.BoundingBox.Width; // äldre version
-            //float relativeIntersectX = Math.Abs((BoundingBox.Center.X - entity.BoundingBox.Center.X) / (float)entity.BoundingBox.Width); //ny test version
-            //float maxBounceAngle = MathHelper.Pi / 3;
-            //float bounceAngle = relativeIntersectX * maxBounceAngle;
-
-            //// Beräkna den nya hastigheten baserat på vinkeln
-            //float newVelocityX = (float)Math.Sin(bounceAngle) * Velocity.Length();
-            //float newVelocityY = -(float)Math.Cos(bounceAngle) * Velocity.Length();
-
-            //// Tilldela den nya hastigheten till Velocity
-            //Velocity = new Vector2(newVelocityX, newVelocityY);
         }
     }
     public override void Draw(SpriteBatch spriteBatch)
