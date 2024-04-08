ef82329ca680179bd9e982be0650f3c20be6b25b
AlbinHadzic <albin_hadzic@hotmail.com>
2024-04-08 14:52:24 +0200

    Fixed the kamikazes to stage2

--- ArkanoidClone/Game1.cs
+++ ArkanoidClone/Game1.cs
@@ -135,7 +135,6 @@ namespace ArkanoidClone
             }
             playerBar.Update(gameTime);
             bricks = brickManager.Update();
-            kamikazeManager.Update(gameTime, playerBar, life);
             foreach (ShitShooter shitShooter in brickManager.ShitShooters)
             {
                 shitShooter.Update(gameTime, playerBar, life);
@@ -144,6 +143,7 @@ namespace ArkanoidClone
             brickManager = ball.DetectCollisionWithBrickOrShitShooter(brickManager);
             playerBar = brickManager.UpdateSizeUps(playerBar, gameTime);
             life = brickManager.UpdateLifeUps(playerBar, gameTime, life);
+            //kamikazeManager.Update(gameTime, playerBar, life);
             currentGameState = brickManager.UpdateStageProgress(currentGameState);
             currentGameState = life.Update(currentGameState);
         }
@@ -196,11 +196,13 @@ namespace ArkanoidClone
                 case GameState.SetUpStage2:
                     ResetObjects();
                     brickManager.SetupStage2();
+
                     currentGameState = nextStageScreen.Update(currentKeyboardState, previousKeyboardState);
                     break;
 
                 case GameState.PlayingStage2:
                     UpdatePlayingLoop(gameTime);
+                    kamikazeManager.Update(gameTime, playerBar, life);
                     PauseAtReset();
                     break;
 
